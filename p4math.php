f13b834d62d93922850c6d261d46e973681051f4
dwesty23 <120140940+dwesty23@users.noreply.github.com>
2023-04-06 13:26:30 -0500

    brace

--- p4math.php
+++ p4math.php
@@ -7,18 +7,19 @@ $cols = $_GET['size'];
 echo "<table border=\"1\">";
         echo '<tr>';
         for ($i=0; $i <= $cols; $i++){
-        if ($i == 0){
-        echo '<td></td>';
-        }else{
-        echo '<td>' .$i.'</td>';
-        }
+            if($i == 0){
+                echo '<td></td>';
+            }
+            else{
+                echo '<td>' .$i.'</td>';
+            }
         }
         for ($r =1; $r <= $rows; $r++){
-
             echo'<tr>';
             echo '<td>'.$r. '</td>';
-            for ($c = 1; $c <= $cols; $c++)
-                echo '<td>' .$c*$r.'</td>';
+            for ($c = 1; $c <= $cols; $c++){
+                echo '<td>' .$c*$r.'</td>'; 
+            }
            echo '</tr>'; // close tr tag here
 
         }
