6e5330757ee4c46289091a85ba75175e6fb3c397
dwesty23 <120140940+dwesty23@users.noreply.github.com>
2023-04-06 15:06:06 -0500

    fixing?

--- p4math.php
+++ p4math.php
@@ -1,29 +1,32 @@
-<!DOCTYPE html>  
-<html>  
-<body>  
+<!DOCTYPE html>
+<html>
+<body>
 <?php
-$rows = $_GET['size'];
-$cols = $_GET['size'];
-echo "<table border=\"1\">";
-        echo '<tr>';
-        for ($i=0; $i <= $cols; $i++){
-            if($i == 0){
-                echo '<td></td>';
-            }
-            else{
-                echo '<td>' .$i.'</td>';
-            }
+    $rows = $_GET['size'];
+    $cols = $_GET['size'];
+    echo "<table border=\"1\">";
+    echo '<tr>';
+    for ($i=0; $i <= $cols; $i++){
+        if($i == 0){
+            echo '<td></td>';
         }
-        for ($r =1; $r <= $rows; $r++){
-            echo'<tr>';
-            echo '<td>'.$r. '</td>';
-            for ($c = 1; $c <= $cols; $c++){
-                echo '<td>' .$c*$r.'</td>'; 
-            }
-           echo '</tr>'; // close tr tag
+            
+        else{
+            echo '<td>' .$i.'</td>';
         }
-  echo"</table>";
-?>    
-</table>  
-</body>  
-</html>  
+            
+    }
+    for ($r =1; $r <= $rows; $r++){
+        echo'<tr>';
+        echo '<td>'.$r. '</td>';
+        for ($c = 1; $c <= $cols; $c++)
+            echo '<td>' .$c*$r.'</td>';
+        echo '</tr>'; // close tr tag here
+        }
+    echo"</table>";
+
+
+?>
+</table>
+</body>
+</html>
